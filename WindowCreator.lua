03f340ebc6e13ded89718e508a4be8cfc92a29ea
MarkDus <79055166+MarkDus@users.noreply.github.com>
2022-05-24 15:21:35 -0400

    Create WindowCreator.lua

---
+++ WindowCreator.lua
@@ -0,0 +1,16 @@
+local window = {}
+
+function window.new()
+    return setmetatable(lib, { __index = {
+        -- Index something here later bitch --
+    }});
+end;
+
+function window:CreateFrame(Position, Size, Color, Parent)
+    local frame = Instance.new("Frame", Parent);
+    frame.Position = Position;
+    frame.Size = Size;
+    frame.BackgroundColor = Color;
+end
+
+return window
